616962f5a3ebe7e06155ec6c8b54d3efc7ef979f
consolelogfuku <fuku1223sugawa@gmail.com>
2022-08-20 17:48:07 +0900

    8/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#dstroy"
+
+  namespace :admin do
+    resources :users
+  end
   root to: "tasks#index" # localhost:3000にアクセスすると"tasks/index"に繋がる
   resources :tasks # ルーティングの一括設定
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
