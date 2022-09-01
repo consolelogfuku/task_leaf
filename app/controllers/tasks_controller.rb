b2824daf7fd9e2ddee64a3e26fbd7526edf3825b
consolelogfuku <fuku1223sugawa@gmail.com>
2022-09-01 22:22:03 +0900

    9/1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,8 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task.destroy
-    redirect_to tasks_path, notice: "タスク「#{task.name}」を削除しました"
+    @task.destroy
+    redirect_to tasks_path, notice: "タスク「#{@task.name}」を削除しました"
   end
   
   private
